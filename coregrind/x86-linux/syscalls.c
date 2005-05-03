svn2github/valgrind

commit 985fabb97295f59db3135bdd1ac627744a8abfb0

Create a new module, "sigframe", responsible for creating/destroying signal frames.  This commit looks worse than it is -- really just a load of moving-code-around.
This is the first multiple-implementation module, in that it has a
single interface (pub_core_sigframe.h) but multiple implementations,
depending on the os-cpu pair.  All the grotty details are hidden in
the implementation in m_sigframe/; callers need be aware only of the
interface.  Yay.



git-svn-id: svn://svn.valgrind.org/valgrind/trunk@3556 a5019735-40e9-0310-863c-91ae7b9d1cf9

coregrind/x86-linux/syscalls.c
@@ -35,7 +35,7 @@
 
 #include "core.h"
 #include "ume.h"                /* for jmp_with_stack */
-
+#include "pub_core_sigframe.h"
 
 /* ---------------------------------------------------------------------
    Stacks, thread wrappers, clone
@@ -590,7 +590,7 @@ PRE(sys_sigreturn, Special)
       something goes wrong in the sigreturn */
    VGA_(restart_syscall)(&tst->arch);
 
-   VGA_(signal_return)(tid, False);
+   VG_(sigframe_destroy)(tid, False);
 
    /* Keep looking for signals until there are none */
    VG_(poll_signals)(tid);
@@ -611,7 +611,7 @@ PRE(sys_rt_sigreturn, Special)
       something goes wrong in the sigreturn */
    VGA_(restart_syscall)(&tst->arch);
 
-   VGA_(signal_return)(tid, True);
+   VG_(sigframe_destroy)(tid, False);
 
    /* Keep looking for signals until there are none */
    VG_(poll_signals)(tid);

configure.in
@@ -394,6 +394,7 @@ AC_OUTPUT(
    auxprogs/Makefile
    coregrind/Makefile 
    coregrind/demangle/Makefile 
+   coregrind/m_sigframe/Makefile 
    coregrind/amd64/Makefile
    coregrind/arm/Makefile
    coregrind/x86/Makefile

coregrind/Makefile.am
@@ -4,8 +4,13 @@ include $(top_srcdir)/Makefile.core-AM_CPPFLAGS.am
 ## When building, we are only interested in the current arch/OS/platform.
 ## But when doing 'make dist', we are interested in every arch/OS/platform.
 ## That's what DIST_SUBDIRS specifies.
-SUBDIRS      = $(VG_ARCH)     $(VG_OS)     $(VG_PLATFORM)     demangle .
-DIST_SUBDIRS = $(VG_ARCH_ALL) $(VG_OS_ALL) $(VG_PLATFORM_ALL) demangle .
+SUBDIRS = \
+	$(VG_ARCH)     $(VG_OS)     $(VG_PLATFORM)     \
+	m_sigframe demangle .
+
+DIST_SUBDIRS = \
+	$(VG_ARCH_ALL) $(VG_OS_ALL) $(VG_PLATFORM_ALL) \
+	m_sigframe demangle .
 
 AM_CPPFLAGS += -DVG_LIBDIR="\"$(valdir)"\" -I$(srcdir)/demangle \
 		-DKICKSTART_BASE=@KICKSTART_BASE@ \
@@ -87,6 +92,7 @@ stage2_SOURCES = \
 ## libplatform.a must be before libarch.a and libos.a, it seems.
 stage2_extra= \
 	demangle/libdemangle.a \
+	m_sigframe/libsigframe.a \
 	${VG_PLATFORM}/libplatform.a \
 	${VG_ARCH}/libarch.a \
 	${VG_OS}/libos.a \

coregrind/amd64/Makefile.am
@@ -21,7 +21,6 @@ libarch_a_SOURCES = \
 	cpuid.S \
 	helpers.S \
 	dispatch.S \
-	signals.c \
 	jmp_with_stack.c \
 	state.c
 

coregrind/amd64/core_arch.h
@@ -127,11 +127,6 @@ struct _ThreadArchAux {
 // Base address of client address space.
 #define VGA_CLIENT_BASE       0x0ul
 
-/* ---------------------------------------------------------------------
-   Signal stuff (should be plat)
-   ------------------------------------------------------------------ */
-
-void VGA_(signal_return)(ThreadId tid, Bool isRT);
 
 #endif   // __AMD64_CORE_ARCH_H
 

coregrind/arm/Makefile.am
@@ -18,7 +18,6 @@ CLEANFILES = stage2.lds
 
 libarch_a_SOURCES = \
 	dispatch.S \
-	signals.c \
 	state.c
 
 if USE_PIE

coregrind/core.h
@@ -1687,13 +1687,6 @@ extern Int  VGA_(ptrace_setregs_from_tst) ( Int pid, ThreadArchState* arch );
 // Used by leakcheck
 extern void VGA_(mark_from_registers)(ThreadId tid, void (*marker)(Addr));
 
-// Signal stuff
-extern void VGA_(push_signal_frame) ( ThreadId tid, Addr sp_top_of_frame,
-                                      const vki_siginfo_t *siginfo,
-                                      void *handler, UInt flags,
-                                      const vki_sigset_t *mask,
-				      void *restorer );
-
 ////typedef struct _ThreadArchAux ThreadArchAux;
 
 

coregrind/m_sigframe/Makefile.am
@@ -1,0 +1,18 @@
+include $(top_srcdir)/Makefile.all.am
+include $(top_srcdir)/Makefile.core-AM_CPPFLAGS.am
+
+AM_CFLAGS = $(WERROR) -Wmissing-prototypes -Winline -Wall -Wshadow -O -g
+
+EXTRA_DIST = \
+	README_SIGFRAME.txt
+
+noinst_LIBRARIES = libsigframe.a
+
+libsigframe_a_SOURCES = \
+	sigframe-@VG_PLATFORM@.c
+
+if USE_PIE
+libsigframe_a_CFLAGS = $(AM_CFLAGS) -fpie
+else
+libsigframe_a_CFLAGS = $(AM_CFLAGS)
+endif

commit 5bcde920fc7f155b1eba56d6cd973a6c1c3504ba

Fix stupid bug I introduced in r3556, which broke handling of RT signals.  Sigh.
git-svn-id: svn://svn.valgrind.org/valgrind/trunk@3610 a5019735-40e9-0310-863c-91ae7b9d1cf9

coregrind/x86-linux/syscalls.c
@@ -613,7 +613,7 @@ PRE(sys_rt_sigreturn, Special)
       something goes wrong in the sigreturn */
    VGA_(restart_syscall)(&tst->arch);
 
-   VG_(sigframe_destroy)(tid, False);
+   VG_(sigframe_destroy)(tid, True);
 
    /* Keep looking for signals until there are none */
    VG_(poll_signals)(tid);